5bcfaa60d2530a2606461e6324bfd650b7d8db68
Morgan Powers <powers.morgan@ymail.com>
2020-04-20 12:15:48 -0400

    Instruction screen

--- grass.h
+++ grass.h
@@ -5,10 +5,10 @@
 //
 //	grass, 240x160@8, 
 //	+ palette 256 entries, not compressed
-//	+ bitmap not compressed
+//	+ 600 tiles not compressed
 //	Total size: 512 + 38400 = 38912
 //
-//	Time-stamp: 2020-04-20, 12:07:34
+//	Time-stamp: 2020-04-20, 12:15:29
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -17,8 +17,8 @@
 #ifndef GRIT_GRASS_H
 #define GRIT_GRASS_H
 
-#define grassBitmapLen 38400
-extern const unsigned short grassBitmap[19200];
+#define grassTilesLen 38400
+extern const unsigned short grassTiles[19200];
 
 #define grassPalLen 512
 extern const unsigned short grassPal[256];
